59520291cb458eb0f55cf4281eef983a75c2bd37
MastersChrisTBN <72880143+MastersChrisTBN@users.noreply.github.com>
2020-10-25 13:18:02 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt-get update -y && apt-get upgrade -y && git clone https://github.com/mtdlaw88/Win.git && apt-get install python-pip -y && apt-get install python3-pip -y && pip3 install requests && pip3 install colorama && cd Win && apt-get install nano && nano config.json
+apt-get update -y && apt-get upgrade -y && git clone https://github.com/mtdlaw88/999diceBot.git && apt-get install python-pip -y && apt-get install python3-pip -y && pip3 install requests && pip3 install colorama && cd Win && apt-get install nano && nano config.json
