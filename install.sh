a12da6c34c66a190c0c574f584460ed60463e432
MastersChrisTBN <72880143+MastersChrisTBN@users.noreply.github.com>
2020-10-25 13:24:32 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
+echo"SELAMAT DATANG,,, "
 apt-get update -y && apt-get upgrade -y && git clone https://github.com/mtdlaw88/999diceBot.git && apt-get install python-pip -y && apt-get install python3-pip -y && pip3 install requests && pip3 install colorama && cd 999diceBot && apt-get install nano && nano config.json
