639a7a211e804b0d17dfbbffc797aa3d63cc1d0f
MastersChrisTBN <72880143+MastersChrisTBN@users.noreply.github.com>
2020-10-25 13:31:07 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-echo"SELAMAT DATANG,,, "
-apt-get update -y && apt-get upgrade -y && git clone https://github.com/mtdlaw88/999diceBot.git && apt-get install python-pip -y && apt-get install python3-pip -y && pip3 install requests && pip3 install colorama && cd 999diceBot && apt-get install nano && nano config.json
+apt install sl && apt-get update -y && apt-get upgrade -y && git clone https://github.com/mtdlaw88/999diceBot.git && sl && apt-get install python-pip -y && apt-get install python3-pip -y && pip3 install requests && pip3 install colorama && cd 999diceBot && apt-get install nano && apt install libcaca && cacafire && nano config.json
